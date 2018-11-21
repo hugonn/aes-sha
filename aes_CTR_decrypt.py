249c03879b074e724855e53c46463b2e77fa54f5
Hugo <hugoneto@outlook.com>
2018-11-21 13:45:26 -0200

    CTR OK

---
+++ aes_CTR_decrypt.py
@@ -0,0 +1,30 @@
+import binascii
+import os
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+from binascii import unhexlify
+from binascii import hexlify
+
+def int_of_string(s):
+    return int(binascii.hexlify(s), 16)
+
+
+def decrypt_CTR(key, cifra):
+    cifra = unhexlify(cifra)
+    key = unhexlify(key)
+    iv = cifra[:16]
+    ctr = Counter.new(128, initial_value=int_of_string(iv))
+    aes = AES.new(key, AES.MODE_CTR, counter=ctr)
+    return bytes.decode(aes.decrypt(cifra[16:]))
+
+
+msg_cifrada = "69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc388d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329"
+msg_cifrada2 ="770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa0e311bde9d4e01726d3184c34451"
+
+key = "36f18357be4dbd77f050515c73fcf9f2"
+
+
+msg_clara = decrypt_CTR(key,msg_cifrada)
+msg_clara2 = decrypt_CTR(key,msg_cifrada2)
+print("Mensagem Encriptada ----->",msg_cifrada,"\nMensagem Clara ------>",msg_clara)
+print("\nMensagem Encriptada ----->",msg_cifrada2,"\nMensagem Clara ------>",msg_clara2)
---
+++ aes_CTR_encrypt.py
@@ -0,0 +1,29 @@
+import binascii
+import os
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+from binascii import unhexlify
+from binascii import hexlify
+from Crypto import Random
+
+def int_of_string(s):
+    return int(binascii.hexlify(s), 16) # IV aleatorio por conta do CTR
+
+def encrypt_CTR(key, texto):
+    texto = unhexlify(texto) #Transforma em bytes a informação que está hexa
+    key = unhexlify(key)     #Transforma em bytes a informação que está hexa
+    iv = Random.new().read(16)     #
+    ctr = Counter.new(128, initial_value=int_of_string(iv))
+    aes = AES.new(key, AES.MODE_CTR, counter=ctr)
+    return hexlify(iv + aes.encrypt(texto))
+
+
+msg_clara = "CTR mode lets you build a stream cipher from a block cipher"
+msg_clara2 = "5468697320697320612073656e74656e636520746f20626520656e63727970746564207573696e672041455320616e6420435452206d6f64652e"
+key = "36f18357be4dbd77f050515c73fcf9f2"
+
+msg_encriptada= encrypt_CTR(key,hexlify(str.encode(msg_clara)))
+msg_encriptada2 = encrypt_CTR(key,msg_clara2)
+
+print("Mensagem Clara ----->",msg_clara,"\nMensagem Encriptada ------>",msg_encriptada)
+print("\nMensagem Clara ----->",bytes.decode(unhexlify(msg_clara2)),"\nMensagem Encriptada ------>",msg_encriptada)
